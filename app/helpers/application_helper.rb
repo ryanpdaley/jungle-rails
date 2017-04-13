4aca2fa1793fb1ed9a066b4052d90a524ad283d8
Ryan Daley <rpdaley@gmail.com>
2017-04-13 19:15:15 +0000

    order summary done

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def get_order_items(order_id)
+    all_line_items = []
+    order = Order.find(order_id)
+    LineItem.where(order_id: order_id).find_each do |line_item|
+      all_line_items << line_item
+    end
+    all_line_items
+  end
 end
